a75dfe50655cd4313dc6ff21f32a860da5500920
Juwon <hardejuwon@gmail.com>
2023-07-05 18:23:00 +0100

    fix linters

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,5 +3,6 @@ class Business < ApplicationRecord
   has_many :category_businesses, dependent: :destroy
   has_many :categories, through: :category_businesses
 
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Amount must be a number' }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Amount
+                            must be a number' }
 end
